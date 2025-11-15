4c72b39a58db9525f07ba97ce7a00a800d4890b1
yphb1407 <yanhuertabecerra@gmail.com>
2025-11-15 09:08:40 -0500

    Add files via upload

---
+++ codigo semana 12/src/codigo/semana/pkg12/Recursividad.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package codigo.semana.pkg12;
+
+/**
+ *
+ * @author YanHB
+ */
+public class CodigoSemana12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
